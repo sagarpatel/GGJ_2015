a70b12d6a290c1b0faf06673ba7fd317bca2b950
Sagar Patel <17.sagar.patel@gmail.com>
2015-01-25 00:20:15 +0900

    Implemented ball position/rotation transition coroutine

--- GGJ2015_UnityFolder/Assets/Scripts/Ball.cs
+++ GGJ2015_UnityFolder/Assets/Scripts/Ball.cs
@@ -3,12 +3,15 @@ using System.Collections;
 
 public class Ball : MonoBehaviour 
 {
+	public AnimationCurve transitionCurve;
 	public float speed = 1.0f;
 
+	bool isTransitioning = false;
 
 	void Update()
 	{
-		transform.position += transform.up * speed * Time.deltaTime;
+		if(isTransitioning == false)
+			transform.position += transform.up * speed * Time.deltaTime;
 
 	}
 
@@ -19,10 +22,44 @@ public class Ball : MonoBehaviour
 
 		if(other.gameObject.CompareTag("Player") == true)
 		{
-			transform.position = other.gameObject.transform.position;
-			transform.up = other.gameObject.transform.up;
+			Vector3 targetPos = other.gameObject.transform.position;
+			Quaternion targetRot = other.gameObject.transform.rotation;
+
+			StopAllCoroutines();
+			StartCoroutine(TransitionToCenter(targetPos, targetRot));
+
 		}
 
 	}
+
+	IEnumerator TransitionToCenter(Vector3 targetPos, Quaternion targetRot)
+	{
+		isTransitioning = true;
+
+		float timeDuration = 0.5f;
+		float timeCounter = 0;
+
+		Vector3 startPos = transform.position;
+		Quaternion startRot = transform.rotation;
+
+		while( timeCounter < timeDuration)
+		{
+			float step = timeCounter/timeDuration;
+			step = transitionCurve.Evaluate(step);
+
+			transform.position = Vector3.Lerp(startPos, targetPos,step);
+			transform.rotation = Quaternion.Slerp(startRot, targetRot, step);
+
+			timeCounter += Time.deltaTime;
+			yield return null;
+		}
+
+		transform.position = targetPos;
+		transform.rotation = targetRot;
+
+		isTransitioning = false;
+
+	}
+
 
 }
