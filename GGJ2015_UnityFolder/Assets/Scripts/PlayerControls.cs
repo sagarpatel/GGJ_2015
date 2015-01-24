2cc4c571edb173492bc765556183ae95475bec3a
Sagar Patel <17.sagar.patel@gmail.com>
2015-01-24 19:53:15 +0900

    Starting controls for new game

---
+++ GGJ2015_UnityFolder/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour 
+{
+	public KeyCode controlKey;
+
+
+	public void Update()
+	{
+
+		if(Input.GetKeyDown(controlKey) == true)
+		{
+			transform.Rotate(new Vector3(0,0,-90));
+
+		}
+
+	}
+
+}
